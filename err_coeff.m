1a30e70bacb8c291d0dc6f3ed1292ce8a984ffab
Mohammad Rowshan <37102311+mohammad-rowshan@users.noreply.github.com>
2022-02-09 13:42:55 +1100

    Update err_coeff.m

--- err_coeff.m
+++ err_coeff.m
@@ -5,7 +5,7 @@ function [dmin, A_dmin] = err_coeff(I,N)
   for i = B'
     Ki_size = n - d;
     for x = find(dec2bin(I(i),n)-'0'==1)
-      ii = dec2bin(bitxor(N-1,I(i)))-'0';
+      ii = dec2bin(bitxor(N-1,I(i)),n)-'0';
       Ki_size = Ki_size + sum(ii(1:x-1));
     end
     A_dmin = A_dmin + 2ˆKi_size;
