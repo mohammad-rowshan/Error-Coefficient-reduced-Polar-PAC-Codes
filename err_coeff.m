bb14ff9d3f137e97ed04f71cd8d06ff3b6dd81d4
Mohammad Rowshan <37102311+mohammad-rowshan@users.noreply.github.com>
2022-04-14 17:34:05 +1000

    Update err_coeff.m

--- err_coeff.m
+++ err_coeff.m
@@ -1,3 +1,20 @@
+# Polar Code Enumerator ###########################################################
+#
+# Copyright (c) 2021, Mohammad Rowshan
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, 
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, 
+#   this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# Freely distributed for educational and research purposes
+###################################################################################
+
 function [dmin, A_dmin] = err_coeff(I,N)
   d = min(sum(dec2bin(I)-'0',2));
   dmin = 2ˆd; n = log2(N); A_dmin = 0;
